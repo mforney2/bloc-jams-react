9a475448006771d1ddf0ae215c7545a190674b73
Michael Forney <46942273+mforney2@users.noreply.github.com>
2019-03-13 16:22:57 -0700

    Added music functionality.

--- src/components/Album.js
+++ src/components/Album.js
@@ -7,12 +7,42 @@ class Album extends Component {
     super(props);
 
     const album = albumData.find( album => {
-      return album.slug === this.props.match.params.slug
+      return album.slug === this.props.match.params.slug;
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true })
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false })
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song })
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) {this.setSong(song)}
+      this.play();
+    }
   }
 
   render () {
@@ -34,10 +64,11 @@ class Album extends Component {
           </colgroup>
           <tbody>
           {
-            this.state.album.songs.map((song, parseInt) =>
-            <Link to={`/album/${song.slug}`} key={parseInt}>
-              <tr key={song.number}>{parseInt +1}</tr>
-              <tr key={song.length}>{song.duration}</tr>
+            this.state.album.songs.map((song, index) =>
+            <Link to={`/album/${song.slug}`} key={index}>
+              <tr key={index} onClick={() => this.handleSongClick(song)}>{index +1}</tr>
+              <tr key={index.name}>{ song.title }</tr>
+              <tr key={index.duration}>{song.duration}</tr>
             </Link>
             )
           }
